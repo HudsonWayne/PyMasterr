1a6909eb51894f4a68261d46ee55df59f5530a75
HudsonWayne <hudsonnbenhuraa@gmail.com>
2025-04-11 11:56:58 +0200

    initial commit

---
+++ calculatorr/calculator.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, Entry, Button, StringVar
+
+class Calculator:
+    def __init__(self, root):
+        master.title("Calculator")
+        master.geomtry('357x420+0+0')
+        master.config(bg='grey')
+        master.resizable(False,False)
+
+root=Tk()
+root.mainloop()
