38d015c8ef81fbbd69e0819ce79b08cca74ee691
HudsonWayne <hudsonnbenhuraa@gmail.com>
2025-04-11 12:03:04 +0200

    styling the calculator

--- calculatorr/calculator.py
+++ calculatorr/calculator.py
@@ -6,6 +6,11 @@ class Calculator:
         master.geomtry('357x420+0+0')
         master.config(bg='grey')
         master.resizable(False,False)
+        
+        self.equation = StringVar()
+        self.entry_value = ''
+        Entry(width=17,bg= '#fff',font=())
+        
 
 root=Tk()
 root.mainloop()
