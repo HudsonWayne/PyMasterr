2c95469b6dc77555699bd8f477395333e5e78ced
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-11 12:33:10 +0200

    added values

--- calculatorr/calculator.py
+++ calculatorr/calculator.py
@@ -15,6 +15,10 @@ class Calculator:
         self.entry_value+=str(value)
         self.equation.set(self.entry_value)
         
+    def clear(self):
+        self.entry_value = ''
+        self.equation.set(self.entry_value)
+        
 
 root=Tk()
 root.mainloop()
